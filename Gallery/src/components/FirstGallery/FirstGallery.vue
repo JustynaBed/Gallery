ef9246ecb2594b0c32506f47c0df00d51076205e
JustynaBed <justyna.bednarko@gmail.com>
2018-04-11 08:59:22 +0200

    change a image

--- Gallery/src/components/FirstGallery/FirstGallery.vue
+++ Gallery/src/components/FirstGallery/FirstGallery.vue
@@ -35,7 +35,7 @@
                     {id:2, title: 'Img2', src: require("../../assets/img/img3.jpg")},
                     {id:3, title: 'Img3', src: require("../../assets/img/img4.jpg")},
                     {id:4, title: 'Img4', src: require("../../assets/img/img5.jpg")},
-                    {id:5, title: 'Img5', src: require("../../assets/img/img2.jpg")},
+                    {id:5, title: 'Img5', src: require("../../assets/img/img11.jpg")},
                     {id:6, title: 'Img6', src: require("../../assets/img/img6.jpg")},
                     {id:7, title: 'Img7', src: require("../../assets/img/img2.jpg")},
                     {id:8, title: 'Img8', src: require("../../assets/img/img7.jpg")}
